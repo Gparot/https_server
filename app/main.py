49c3f9fafceed5bc77a61e1d30627ffd92e2182d
gparot <gparotleon@alumni.unav.es>
2024-08-06 00:03:53 -0400

    pass1st stage

--- app/main.py
+++ app/main.py
@@ -1,17 +1,9 @@
 
+
 import socket
-
-
 def main():
-    
+  
     print("Logs from your program will appear here!")
-
-    
-    
-server_socket = socket.create_server(("localhost", 4221),reuse_port=True)
-server_socket.accept() # wait for client
-
-
-if __name__ == "__main__":
-    main()
    
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.accept() 
